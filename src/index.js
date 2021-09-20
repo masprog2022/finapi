476f6bb9b8325b3a1b61e47b5ce938c818437532
masprog2022 <masprog2022@gmail.com>
2021-09-20 15:03:15 +0100

    created deposit account

--- src/index.js
+++ src/index.js
@@ -58,4 +58,19 @@ app.get("/statement",verifyExistsAccountCPF, (request, response) => {
 
 })
 
+app.post("/deposit",verifyExistsAccountCPF, (request, response)=>{
+   const { description, amount } = request.body;
+
+   const { customer } = request;
+
+   const statementOperation = {
+       description,
+       amount,
+       created_at: new Date(),
+       type: "credit"
+   }
+   customer.statement.push(statementOperation);
+   return response.status(201).send();
+})
+
 app.listen(3333);
