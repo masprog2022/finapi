f931c14d3385a7492bf09768a94256f3296fdc54
masprog2022 <masprog2022@gmail.com>
2021-09-20 11:50:54 +0100

    Created account

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
+const { application } = require("express");
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const api = express();
+
+api.use(express.json());
+
+const customers = [];
+
+api.post("/account", (request, response) =>{
+    const { cpf, name } = request.body;
+    const id = uuidv4();
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    });
+
+    return response.status(201).send();
+
+} )
 
 api.listen(3333);
